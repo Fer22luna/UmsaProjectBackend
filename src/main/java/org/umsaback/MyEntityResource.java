5bd3f717231e8d026f35c502f42dd8c482fa4dbd
Fernando G. Luna <fer22luna@gmail.com>
2024-06-05 16:21:44 -0300

    estructura basica

---
+++ src/main/java/org/umsaback/MyEntityResource.java
@@ -0,0 +1,6 @@
+package org.umsaback;
+
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+
+public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
+}
