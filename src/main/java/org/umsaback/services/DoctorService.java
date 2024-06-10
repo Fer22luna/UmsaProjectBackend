8bdcece4ebca7fdeb454962dafec89608bf6c1c1
Tomas Rodriguez Zoni <tomasrodriguezzoni2412@gmail.com>
2024-06-09 22:18:36 -0300

    ultimos cambios
    Agrego los ultimos cambios incluyendo buscarPorEspecialidad y poniendo clases con response

--- src/main/java/org/umsaback/services/DoctorService.java
+++ src/main/java/org/umsaback/services/DoctorService.java
@@ -2,6 +2,7 @@ package org.umsaback.services;
 
 import java.util.List;
 
+import org.umsaback.enums.Especialidad;
 import org.umsaback.models.entities.Doctor;
 import org.umsaback.repositories.DoctorRepository;
 
@@ -17,20 +18,29 @@ public class DoctorService {
 	DoctorRepository doctorRepository;
 	
 	public Response listarDoctores(){
-		return Response.ok(doctorRepository.listAll())
-				.build();
+		return Response.ok(doctorRepository.listAll()).build();
 	}
 	
 	public Response crearDoctor(Doctor nuevoDoctor) {
 		doctorRepository.persist(nuevoDoctor);
-		return Response.ok(nuevoDoctor)
-				.build();
+		return Response.ok(nuevoDoctor).build();
 	}
 	
 	public Response getByName(@PathParam("nombre") String nombre) {
 		List<Doctor> doctores = doctorRepository.findByName(nombre);
 		return Response.ok(doctores).build();
 	}
+	
+	public Response getByEspecialidad(@PathParam("especialidad") Especialidad especialidad) {
+		List<Doctor> doctores = doctorRepository.findByEspecialidad(especialidad);
+		return Response.ok(doctores).build();
+	}
+	
+	/*
+	 * public Response deleteByDni(@PathParam("dni") String dni) { booleran
+	 * eliminado = doctorRepository.delete(dni); return Response.ok(dni).build(); }
+	 */
+	
 	
 	
 	
